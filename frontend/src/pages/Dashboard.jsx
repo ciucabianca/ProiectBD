a6d169fb28a6c40e0f473c480993ff43099ccf95
Ciuca Bianca <ciuca.bianca@yahoo.com>
2021-12-25 21:48:25 +0200

    refactoring dashboard buttons

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,33 +1,44 @@
 import { Layout } from "../components/Layout";
 import { Link } from "react-router-dom";
+import { isAuth } from "../helpers/isAuth";
 
 export const Dashboard = (props) => {
+  const renderLogout = () => {
+    return (
+      <Link
+        className="btn btn-danger m-2 px-5"
+        to="/"
+        onClick={() => {
+          console.log("logged out");
+          localStorage.removeItem("authToken");
+        }}>
+        Log Out
+      </Link>
+    );
+  };
+
+  const renderLinkButton = (label, path) => {
+    return (
+      <Link className="btn btn-primary m-2 px-5" to={`/${path}`}>
+        {label}
+      </Link>
+    );
+  };
+
   return (
     <Layout>
       <h1 className="text-center">
-        {localStorage.authToken ? "Welcome!" : "Cars on Demand"}
+        {isAuth() ? "Welcome!" : "Cars on Demand"}
       </h1>
       <div className="d-flex justify-content-center mt-3">
         <div className="d-flex flex-column">
-          <Link
-            className="btn btn-primary m-2"
-            to="/login"
-            style={{ opacity: 1.3 }}>
-            Login
-          </Link>
-          <Link className="btn btn-primary m-2" to="/register">
-            Register
-          </Link>
-          <Link
-            className="btn btn-danger m-2 px-5"
-            to="/"
-            onClick={() => {
-              console.log("logged out");
-              localStorage.removeItem("authToken");
-              window.location.href = "/login";
-            }}>
-            Log Out
-          </Link>
+          {isAuth()
+            ? renderLinkButton("See Available Cars", "catalog")
+            : renderLinkButton("Login", "login")}
+          {isAuth()
+            ? renderLinkButton("My Rentals", "rentals")
+            : renderLinkButton("Register", "register")}
+          {isAuth() && renderLogout()}
         </div>
       </div>
     </Layout>
