6098aafe234797192bf2d356d336e3703365d56a
Ciuca Bianca <ciuca.bianca@yahoo.com>
2022-01-16 20:19:56 +0200

    Connect to backend user update

--- frontend/src/Tasks.js
+++ frontend/src/Tasks.js
@@ -2,7 +2,9 @@
 Cerinte:
 - 2 Insert in tabele diferite - DONE (Users & Rentals)
 - 2 Update-uri
+  - Update rental dates
 - 2 Delete-uri
+  - Delete rental
 - 6 Interogari w Join
   - Rentals:
     - Filter by UserId
@@ -14,4 +16,9 @@ Cerinte:
     - Filter by Location
     - Filter by CarId
 - 4 Interogari w Subcereri
+
+Top 3 cele mai inchiriate Modele
+Top 3 cele mai dorite culori
+Modelul de masina cu cel mai mare venit
+select carid from rentals where locationid in ALL(select locationId from location where locationCity == '"")
 */
--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export const loginAction = async (credentials) => {
   try {
@@ -43,8 +44,49 @@ export const getUser = async () => {
       headers: { Authorization: `bearer ${localStorage.authToken}` },
     });
     return res.data;
+  } catch (err) {
+    console.log("error", err);
+  }
+  return undefined;
+};
+
+export const getUserById = async (userId) => {
+  try {
+    const res = await axios.get(`/api/users/${userId}`, {
+      headers: { Authorization: `bearer ${localStorage.authToken}` },
+    });
+
+    return res.data;
   } catch {
     console.log("error");
   }
   return undefined;
+};
+
+export const updateUser = async (userId, { firstName, lastName }) => {
+  try {
+    const res = await axios.put(
+      `/api/users/${userId}`,
+      { firstName, lastName },
+      {
+        headers: { Authorization: `bearer ${localStorage.authToken}` },
+      }
+    );
+    toast.success("Update Succesful!");
+    return res.data;
+  } catch (err) {
+    console.log("error", err);
+    return undefined;
+  }
+};
+
+export const deleteUser = async (userId) => {
+  try {
+    await axios.delete(`/api/users/${userId}`, {
+      headers: { Authorization: `bearer ${localStorage.authToken}` },
+    });
+    toast.success("Delete Succesful!");
+  } catch (err) {
+    console.log("error", err);
+  }
 };
